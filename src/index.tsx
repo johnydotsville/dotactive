ff4022912d8a2c22871d9b09fca48b79fd6e9e8b
johnydotsville <come.as@you.are>
2025-02-20 14:07:46 +0300

    flush

--- src/index.tsx
+++ src/index.tsx
@@ -11,37 +11,13 @@ import { MatchesStorage } from '@domain/database/storage/MatchesStorage';
 
 async function prepare() {
   const database = new MyDatabase(defaultDbConfig);
-  
-  // const connection = await database.getConnection();
 
   const accountId = 56831765;
 
-  // // const matchStorage = new MatchesStorage(connection, "matches");
   const matchStorage = new MatchesStorage(database, "matches");
-  // await matchStorage.init();
-
 
   const ms = new MatchService(matchStorage);
   await ms.init(accountId);
-  // // const matches = await ms.getAllMatches();
-  const matches = await ms.getMatches();
-  console.log(matches);
-
-  // // // const match = await ms.getMatch(8145432965);
-  const match = await ms.getMatches(8145432965);
-  console.log(match);
-
-  let fewMatches = await ms.getMatches(8146456949, 8148280087, 8166687583, 228);
-  console.log(fewMatches);
-
-  // debugger;
-  const reportDelete = await ms.deleteMatches(8146456949, 8148280087, 8166687583);
-  console.log(reportDelete);
-
-  fewMatches = await ms.getMatches(8146456949, 8148280087, 8166687583, 228);
-  console.log(fewMatches);
-
-  const clearReport = await ms.deleteMatches();
 
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
