86ce1fa3ab7e7a2a5433bb6821346c939bf545e1
johnydotsville <come.as@you.are>
2025-02-26 09:38:09 +0300

    flush

--- src/domain/database/storage/ConstantStorage.ts
+++ src/domain/database/storage/ConstantStorage.ts
@@ -1,10 +1,10 @@
 import { FakeModel } from "@domain/services/matches/model/FakeModel";
 import { Storage } from "./Storage";
-import { Match } from "@domain/services/matches/model/match";
+import { StorageName } from "../config/storages/StorageName";
 
 
 export class ConstantStorage extends Storage<FakeModel, string> {
-  public constructor(database: IDBDatabase, storageName: string) {
+  public constructor(database: IDBDatabase, storageName: StorageName) {
     super(database, storageName);
   }
 }
--- src/domain/database/storage/MatchStorage.ts
+++ src/domain/database/storage/MatchStorage.ts
@@ -1,9 +1,10 @@
+import { StorageName } from "../config/storages/StorageName";
 import { Storage } from "./Storage";
 import { Match } from "@domain/services/matches/model/match";
 
 
 export class MatchStorage extends Storage<Match, number> {
-  public constructor(database: IDBDatabase, storageName: string) {
+  public constructor(database: IDBDatabase, storageName: StorageName) {
     super(database, storageName);
   }
 }
