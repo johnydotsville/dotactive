56ee3fbebd0c5b7156d4a6fa98536c7854745ca9
johnydotsville <come.as@you.are>
2025-03-13 13:46:45 +0300

    flush

--- src/components/Welcome/AddToken/AddToken.tsx
+++ src/components/Welcome/AddToken/AddToken.tsx
@@ -2,8 +2,6 @@ import { checkIfTokenValid } from "@domain/services/stratzapi/utils";
 import { CheckAndSaveValue } from "../CheckAndSaveValue/CheckAndSaveValue";
 
 
-//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJTdWJqZWN0IjoiNmVmNzRjMmItMjJiZi00Mzk3LTgyMTUtY2VjYjk3ZDY2YmNkIiwiU3RlYW1JZCI6IjU2ODMxNzY1IiwibmJmIjoxNzE2ODk2ODk5LCJleHAiOjE3NDg0MzI4OTksImlhdCI6MTcxNjg5Njg5OSwiaXNzIjoiaHR0cHM6Ly9hcGkuc3RyYXR6LmNvbSJ9.QoAd60oMIUV4D8N73Lcj6b2MTqc-96vv6PzFcLQqrhg
-
 export const AddToken = () => {
   const checkToken = async (token: string) => {
     return await checkIfTokenValid(token);
@@ -11,6 +9,13 @@ export const AddToken = () => {
 
   const saveToken = async (token: string) => {
     window.localStorage.setItem("current_token", token);
+    const allTokensKey = "all_tokens";
+    const allTokens = window.localStorage.getItem(allTokensKey);
+    if (!allTokens) {
+      window.localStorage.setItem(allTokensKey, token);
+    } else {
+      window.localStorage.setItem(allTokensKey, allTokens + `,${token}`)
+    }
   }
 
   return (
--- src/components/Welcome/CheckAndSaveValue/CheckAndSaveValue.tsx
+++ src/components/Welcome/CheckAndSaveValue/CheckAndSaveValue.tsx
@@ -6,8 +6,8 @@ import * as styles from "./CheckAndSaveValue.module.css";
 
 type CheckAndSaveValueProps = {
   label: string,
-  checkFunc: any,
-  saveFunc: any
+  checkFunc: (value: string) => Promise<boolean>,
+  saveFunc: (value: string) => Promise<void>
 }
 
 
@@ -36,78 +36,15 @@ export const CheckAndSaveValue: React.FC<CheckAndSaveValueProps> = ({label, chec
 
   return (
     <div className={styles.wrapper}>
-      <div className={styles.inputPair}>
+      <div className={styles.flexLine}>
         <label htmlFor="valueForCheck">{label}</label>
-        <input id="valueForCheck" ref={valueRef} className={styles.inputPairText} type="text" disabled={!valueInputEnabled} />
+        <input id="valueForCheck" ref={valueRef} className={styles.inputPairText} 
+               type="text" disabled={!valueInputEnabled} />
       </div>
-      <div className={styles.inputPair}>
+      <div className={styles.flexLine}>
         <button onClick={checkValue}>Проверить</button>
         { valueSaveEnabled && <button onClick={saveValue}>Сохранить</button> }
       </div>
     </div>
   )
 }
-
-// export const AddToken = () => {
-//   const [ tokenInputEnabled, setTokenInputEnabled ] = useState(true);
-//   const [ tokenValid, setTokenValid ] = useState(false);
-  
-//   const tokenRef = useRef<HTMLInputElement | null>(null);
-//   const database: MyDatabase = useContext(DatabaseContext);
-
-//   const api = useMemo(() => {
-//     return new StratzAPI();
-//   }, []);
-
-//   const checkToken = async () => {
-//     setTokenInputEnabled(false);
-//     api.setToken(tokenRef.current.value);
-//     const tokenIsOk = await api.checkToken();
-//     if (tokenIsOk) {
-//       setTokenValid(true);
-//     } else {
-//       setTokenInputEnabled(true);
-//     }
-//   }
-
-//   const saveToken = async () => {
-//     const storage = database.getStorage<TokenStorage>(StorageName.Tokens);
-//     const token: Token = { token: tokenRef.current.value };
-//     const saveTokenResult = await storage.save(token);  // TODO: Сделать мб проверку на уже существующий такой же токен, чтобы не сохранять одинаковые?
-//     const errors = saveTokenResult.error;
-//     const result = saveTokenResult.result[0];
-//     window.localStorage.setItem("current_token", result.token);
-//     setTokenValid(false);
-//     setTokenInputEnabled(true);
-//     tokenRef.current.value = "";
-//   }
-
-//   return (
-//     <div className={styles.wrapper}>
-//       <div className={styles.inputPair}>
-//         <label htmlFor="token">Stratz Token:</label>
-//         <input id="token" ref={tokenRef} className={styles.inputPairText} type="text" disabled={!tokenInputEnabled} />
-//       </div>
-//       <div className={styles.inputPair}>
-//         <button onClick={checkToken}>Проверить</button>
-//         { tokenValid && <button onClick={saveToken}>Сохранить</button> }
-//       </div>
-//     </div>
-//   )
-// }
-
-
-async function saveValidToken(token: string) {
-  
-}
-
-/*
-  Логика компонента:
-  * Есть поле ввода под токен и кнопка Проверить.
-  * Человек вводит токен, нажимает Проверить.
-  * Поле ввода становится disabled, чтобы человек не мог изменить токен, пока не закончится обработка.
-  * Если проверка показывает, что токен невалидный, то поле снова становится доступно для редактирования,
-    чтобы человек мог исправить токен.
-  * Если токен валидный, то поле все еще не доступно для редактирования, но появляется кнопка Сохранить.
-  * После успешного сохранения поле ввода токена очищается и становится доступным для редактирования, а кнопка сохранения исчезает
-*/
